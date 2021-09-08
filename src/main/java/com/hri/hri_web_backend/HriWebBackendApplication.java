facdf06b6704017a3d49b4ff8b6b965368c87494
sieun <31584255+sileeee@users.noreply.github.com>
2021-09-08 15:16:41 +0900

    feat: 프로젝트 생성 (#2)

---
+++ src/main/java/com/hri/hri_web_backend/HriWebBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hri.hri_web_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HriWebBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HriWebBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hri/hri_web_backend/HriWebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hri.hri_web_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HriWebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
