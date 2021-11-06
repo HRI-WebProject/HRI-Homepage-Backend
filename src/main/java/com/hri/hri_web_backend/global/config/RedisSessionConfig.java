816e81fb6c32833982e0b52bf10c6cf25b5631d4
sieun <tldms201@naver.com>
2021-11-06 23:34:09 +0900

    feat: 세션 스토리지로 redis 연동

---
+++ src/main/java/com/hri/hri_web_backend/global/config/RedisSessionConfig.java
@@ -0,0 +1,32 @@
+package com.hri.hri_web_backend.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@EnableRedisHttpSession
+@Configuration
+public class RedisSessionConfig {
+
+	@Value("${spring.redis.session.host}")
+	private String host;
+
+	@Value("${spring.redis.session.port}")
+	private int port;
+
+	@Value("${spring.redis.session.password}")
+	private String password;
+
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
+		RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+		config.setHostName(host);
+		config.setPort(port);
+		config.setPassword(password);
+		return new LettuceConnectionFactory(config);
+	}
+}
