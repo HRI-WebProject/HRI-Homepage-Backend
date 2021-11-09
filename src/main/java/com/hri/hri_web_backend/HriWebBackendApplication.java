682f0f635a9b7ec8199198d03d6a1fe64442879e
박계원 <62559049+Kyewon-Park@users.noreply.github.com>
2021-11-09 13:59:19 +0900

    style: 필요없는 import문 제거

--- src/main/java/com/hri/hri_web_backend/HriWebBackendApplication.java
+++ src/main/java/com/hri/hri_web_backend/HriWebBackendApplication.java
@@ -1,13 +1,7 @@
 package com.hri.hri_web_backend;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.hri.hri_web_backend.domain.Administrator;
-import com.hri.hri_web_backend.service.AdminService;
 
 @SpringBootApplication
 public class HriWebBackendApplication {
