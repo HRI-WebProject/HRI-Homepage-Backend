0d36ab25433421d8a409f540ea9235f71ab773e4
박계원 <62559049+Kyewon-Park@users.noreply.github.com>
2021-11-18 19:10:56 +0900

    [#10] 학술지, 학술대회, 특허, 도서 소개 조회 ,등록 , 수정 및 삭제  (#18)
    * feat: Publication 조회, 생성, 수정 및 삭제
    
    Publication 조회, 생성, 수정 및 삭제
    SuccessResponse, redirect 사용
    
    * refactor: 엔티티 setter추가
    
    * fix: ApplicationContext 오류 고침/ publicationService 테스트 완료
    
    failed to load applicationcontext spring boot test
    => PublicationRepository 에서 _언더바 인식 안됨 => Entity의 publication_type 을 publicationType으로 고침
    
    수정/삭제 테스트 추가 및 실행 완료
    
    * style: publication service/repo 이름 및 테스트 클래스 이름 수정
    
    + else 문 삭제
    
    * Create PublicationFixture.java

---
+++ src/main/java/com/hri/hri_web_backend/service/PublicationService.java
@@ -0,0 +1,63 @@
+package com.hri.hri_web_backend.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.hri.hri_web_backend.domain.Publication;
+import com.hri.hri_web_backend.domain.PublicationType;
+import com.hri.hri_web_backend.dto.PublicationDto;
+import com.hri.hri_web_backend.repository.PublicationRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class PublicationService {
+
+	private final PublicationRepository publicationRepository;
+
+	//생성
+	@Transactional
+	public void savePublication(@Valid PublicationDto publicationDto){
+		Publication publication = Publication.builder()
+			.link(publicationDto.getLink())
+			.publicationType(publicationDto.getPublication_type())
+			.topic(publicationDto.getTopic())
+			.detail(publicationDto.getDetail())
+			.build();
+		Publication saved = publicationRepository.save(publication);
+	}
+
+	//조회
+	public List<Publication> findPublicationsByType(PublicationType type){
+		return publicationRepository.findByPublicationType(type);
+	}
+
+	//수정
+	@Transactional
+	public void updatePublication(PublicationDto publicationDto, Long id) {
+		Optional<Publication> publication = publicationRepository.findById(id);
+		if(publication.isEmpty()) {
+			throw new NullPointerException();
+		}
+		Publication publication1 = publication.get();
+		publication1.setPublicationType(publicationDto.getPublication_type());
+		publication1.setDetail(publicationDto.getDetail());
+		publication1.setLink(publicationDto.getLink());
+		publication1.setTopic(publicationDto.getTopic());
+
+	}
+
+	//삭제
+	@Transactional
+	public void deletePublication(Long id){
+		publicationRepository.deleteById(id); //EmptyResultDataAccessException 발생 가능
+	}
+}
---
+++ src/test/java/com/hri/hri_web_backend/fixture/PublicationFixture.java
@@ -0,0 +1,36 @@
+package com.hri.hri_web_backend.fixture;
+
+import com.hri.hri_web_backend.domain.Member;
+import com.hri.hri_web_backend.domain.Publication;
+import com.hri.hri_web_backend.domain.PublicationType;
+import com.hri.hri_web_backend.global.DegreeEnum;
+
+public class PublicationFixture {
+	public static class Publication1{
+		public static final String TOPIC = "topic ex1";
+		public static final String LINK = "link ex1";
+		public static final String DETAIL = "Detail ex1";
+		public static final PublicationType PUBLICATIONTYPE = PublicationType.JOURNAL;
+
+		public static final Publication publication1 = Publication.builder()
+			.topic(TOPIC)
+			.link(LINK)
+			.detail(DETAIL)
+			.publicationType(PUBLICATIONTYPE)
+			.build();
+	}
+
+	public static class Publication2{
+		public static final String TOPIC = "topic ex2";
+		public static final String LINK = "link ex2";
+		public static final String DETAIL = "Detail ex2";
+		public static final PublicationType PUBLICATIONTYPE = PublicationType.BOOK;
+
+		public static final Publication publication2 = Publication.builder()
+			.topic(TOPIC)
+			.link(LINK)
+			.detail(DETAIL)
+			.publicationType(PUBLICATIONTYPE)
+			.build();
+	}
+}
