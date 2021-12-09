02ebf45f762268f6350b0f0833b0607b4cf01260
sieun <tldms201@naver.com>
2021-12-09 18:37:53 +0900

    feat: 프로젝트 CRUD 테스트코드 추가

---
+++ src/test/java/com/hri/hri_web_backend/fixture/ProjectFixture.java
@@ -0,0 +1,17 @@
+package com.hri.hri_web_backend.fixture;
+
+import com.hri.hri_web_backend.domain.Project;
+
+public class ProjectFixture {
+
+	public static class Project1 {
+		public static final Long ID = 1L;
+		public static final String DETAIL = "project1_detail";
+		public static final String TOPIC = "project1_topic";
+
+		public static final Project PROJECT = Project.builder()
+			.detail(DETAIL)
+			.topic(TOPIC)
+			.build();
+	}
+}
