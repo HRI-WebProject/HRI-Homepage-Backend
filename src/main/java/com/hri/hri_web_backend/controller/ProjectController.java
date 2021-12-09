07a1666b210fc6f4a0906944ca99623d2331d96f
sieun <31584255+sileeee@users.noreply.github.com>
2021-12-09 18:43:12 +0900

    Revert "[#17] 현재 진행 프로젝트 등록, 수정, 삭제 (#24)"
    This reverts commit 64c14cb38e052819ed4caf54d98b8b7316bba26c.

--- src/main/java/com/hri/hri_web_backend/controller/ProjectController.java
+++ src/main/java/com/hri/hri_web_backend/controller/ProjectController.java
@@ -2,20 +2,11 @@ package com.hri.hri_web_backend.controller;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.hri.hri_web_backend.domain.Project;
-import com.hri.hri_web_backend.dto.RegisterProjectDto;
-import com.hri.hri_web_backend.dto.UpdateRequestDto;
 import com.hri.hri_web_backend.global.StatusEnum;
 import com.hri.hri_web_backend.global.SuccessResponse;
 import com.hri.hri_web_backend.service.ProjectService;
@@ -36,35 +27,6 @@ public class ProjectController {
 			.status(StatusEnum.OK)
 			.data(projects)
 			.message("프로젝트 조회 성공")
-			.build();
-	}
-	@ResponseBody
-	@PostMapping("/admin/projects")
-	public SuccessResponse registerProject(@Valid @RequestBody RegisterProjectDto dto){
-		projectService.saveProject(dto);
-		return SuccessResponse.builder()
-			.status(StatusEnum.OK)
-			.message("프로젝트 등록 성공")
-			.build();
-	}
-
-	@ResponseBody
-	@PutMapping("/admin/projects/{id}")
-	public SuccessResponse updateProject(@Valid @RequestBody UpdateRequestDto dto, @PathVariable long id){
-		projectService.changeProjectInfo(dto, id);
-		return SuccessResponse.builder()
-			.status(StatusEnum.OK)
-			.message("프로젝트 수정 성공")
-			.build();
-	}
-
-	@ResponseBody
-	@DeleteMapping("/admin/projects/{id}")
-	public SuccessResponse deleteProject(@PathVariable long id){
-		projectService.deleteProjectInfo(id);
-		return SuccessResponse.builder()
-			.status(StatusEnum.OK)
-			.message("프로젝트 삭제 성공")
 			.build();
 	}
 }
--- src/main/java/com/hri/hri_web_backend/domain/Project.java
+++ src/main/java/com/hri/hri_web_backend/domain/Project.java
@@ -8,11 +8,9 @@ import javax.persistence.Id;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Getter
-@Setter
 public class Project {
 
 	@Id
