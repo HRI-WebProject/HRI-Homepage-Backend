c2d9ee18f0d94dcb826b861994f49c02c688699e
박계원 <62559049+Kyewon-Park@users.noreply.github.com>
2021-12-31 15:16:52 +0900

    feat: data.sql 작성1

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,20 @@
-INSERT INTO member (degree, email, eng_name, graduate, name, photo, research_area) VALUES ('MASTER', 'lweimo@foxmail.com', 'Ae-Ryeong Im', false, '림애령', 'pass', '컴퓨터비전');
+INSERT INTO member (degree, email, eng_name, graduate, name, research_area) VALUES ('PHD', 'one2003@hanmail.net', 'Young-won Cho', false, '조영원', '컴퓨터비전');
+INSERT INTO member (degree, email, eng_name, graduate, name, research_area) VALUES ('PHD', 'sik2230@dongguk.edu', 'Yong-sik Choi', false, '최용식', '모바일로봇, 지능시스템');
+INSERT INTO member (degree, email, eng_name, graduate, name, research_area) VALUES ('MASTER', 'lweimo@foxmail.com', 'Ae-Ryeong Im', false, '림애령', '컴퓨터비전');
+INSERT INTO member (degree, email, eng_name, graduate, name, research_area) VALUES ('MASTER', 'cymothoa@dgu.ac.kr', 'Seung-yoon Yang', false, '양승윤', '지능시스템');
+INSERT INTO member (degree, email, eng_name, graduate, name, research_area) VALUES ('MASTER', 'kangtaepoong@naver.com', 'Hyun-wook Kang', false, '강현욱', '지능시스템');
+INSERT INTO member (degree, email, eng_name, graduate, name, research_area) VALUES ('MASTER', 'skwndbth159@dongguk.edu', 'Woo-jin Jang', false, '장우진', '모바일로봇');
+INSERT INTO member (degree, email, eng_name, graduate, name, research_area) VALUES ('MASTER', 'ktw3388@dgu.ac.kr', 'Tae-won Kang', false, '강태원', '컴퓨터비전, 모바일로봇');
+INSERT INTO member (degree, email, eng_name, graduate, name, research_area) VALUES ('BACHELOR', 'tldms201@dgu.ac.kr', 'Si-eun Lee', false, '이시은', '지능시스템');
+INSERT INTO member (degree, email, eng_name, graduate, name, research_area) VALUES ('BACHELOR', 'sooa9918@dgu.ac.kr', 'Soo-a Jang', false, '장수아', '지능시스템');
+INSERT INTO member (degree, email, eng_name, graduate, name, research_area) VALUES ('BACHELOR', 'didphtmd@gmail.com', 'Bo-seung Yang', false, '양보승', '지능시스템');
+INSERT INTO member (degree, email, eng_name, graduate, name, research_area) VALUES ('BACHELOR', 'lynnp1004@naver.com', 'Lynn Park', false, '박린', '지능시스템');
+INSERT INTO member (degree, email, eng_name, graduate, name, research_area) VALUES ('BACHELOR', 'pkw3136@naver.com', 'Kye-won Park', false, '박계원', '지능시스템');
+
+INSERT INTO professor (name, eng_name, email, bold_detail, detail) VALUES ('정진우', 'Jin-Woo Jung', 'jwjung@dongguk.edu', '동국대학교 컴퓨터공학과/인공지능학과 교수, 컴퓨터공학과 학과장', '국제저명학술지(IJFIS) 편집위원장\n한국지능시스템학회 이사, 한국공학교육회 이사, 한국공학교육인증원 CAC 부위원장\n한국과학기술원(KAIST) 전기및전자공학과 공학사/공학석사\n한국과학기술원(KAIST) 전자전산학과 공학박사\n일본 동경대학교 방문연구원, 미국 Purdue University 방문교수\n국제 저명(SCI급)학술지 게재 50여 편, 학술대회 논문 발표 200여 편, 특허 등록 20여 건, 기술이전 10여 건');
+
+INSERT INTO publication (topic, publication_type) VALUES ('Jin-Gu kang, Yong-Sik Choi, Jin-Woo Jung, “A Method of Enhancing Rapidly-Exploring Random Tree Robot Path Planning Using Midpoint Interpolation,” Sensors, Vol. 11, No. 18, 2021 (SCIE)', 'JOURNAL');
+INSERT INTO publication (topic, publication_type) VALUES ('Jin-Gu kang, Dong-Woo Lim, Yong-Sik Choi, Woo-Jin Jang, Jin-Woo Jung, “Improved RRT-Connect Algorithm Based on Triangular Inequality for Robot Path Planning,” Sensors, Vol. 21, No. 2, 2021 (SCIE)', 'JOURNAL');
+INSERT INTO publication (topic, publication_type) VALUES ('Jin-Woo Jung; Jung-Soo Park; Tae-Won Kang; Jin-Gu Kang; Hyun-Wook Kang. Mobile Robot Path Planning Using a Laser Range Finder for Environments with Transparent Obstacles. Applied Sciences 2020, 10(8), 2799. (SCIE)', 'JOURNAL');
+INSERT INTO publication (topic, publication_type) VALUES ('Jin-Woo Jung; Byung-Chul So; Jin-Gu Kang; Dong-Woo Lim; Yunsik Son. Expanded Douglas–Peucker Polygonal Approximation and Opposite Angle-Based Exact Cell Decomposition for Path Planning with Curvilinear Obstacles. Applied Sciences 2019, 9(4), 638. (SCIE)', 'JOURNAL');
+INSERT INTO publication (topic, publication_type) VALUES ('Gi-Yoon Jeon; Jin-Woo Jung. Water Sink Model for Robot Motion Planning. Sensors 2019, 19(6). (SCIE)', 'JOURNAL');
